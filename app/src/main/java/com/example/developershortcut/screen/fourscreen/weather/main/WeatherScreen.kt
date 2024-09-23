a4a2ba6bc442518b5c4c4b2d4d1b1ccf50938268
unknown <carlos.sala.munoz@gmail.com>
2024-09-23 00:52:25 -0300

    se mejora el splash screen y se actualizan dependencias

--- app/src/main/java/com/example/developershortcut/screen/fourscreen/weather/main/WeatherScreen.kt
+++ app/src/main/java/com/example/developershortcut/screen/fourscreen/weather/main/WeatherScreen.kt
@@ -28,13 +28,14 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.developershortcut.R
 import com.example.developershortcut.screen.fourscreen.weather.domain.model.WeatherModel
 
-
+@Preview
 @Composable
 fun WeatherScreen() {
 
--- app/src/main/java/com/example/developershortcut/screen/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/developershortcut/screen/splashscreen/SplashScreen.kt
@@ -1,17 +1,37 @@
 package com.example.developershortcut.screen.splashscreen
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.developershortcut.R
@@ -23,7 +43,7 @@ fun SplashScreen(navController: NavController) {
 
     LaunchedEffect(key1 = true) {
 
-        delay(300)
+        delay(1000)
         navController.popBackStack()
         navController.navigate(AppScreens.MainScreen.route)
     }
@@ -34,23 +54,48 @@ fun SplashScreen(navController: NavController) {
 @Composable
 fun Splash() {
 
+    var visible by remember { mutableStateOf(false) }
+
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFFF0F0F0), // Color inicial
+                        Color(0xFFE0E0E0), // Color central
+                        Color(0xFFB0B0B0)  // Color final
+                    )
+                )
+            )
+            .padding(top = 0.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-
         Image(
-            painter = painterResource(id = R.drawable.smoke),
-            contentDescription = null
+            painter = painterResource(id = R.drawable.ic_app_shortcut_24),
+            contentDescription = null,
+            modifier = Modifier.size(width = 84.dp, height = 84.dp)
         )
-        Text(
-            text = "Developer Shortcuts",
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold
-        )
-    }
+        Spacer(modifier = Modifier.height(8.dp))
 
+        LaunchedEffect(key1 = true) {
+            // Activa la animación después de un breve retraso
+            visible = true
+            // delay(1000)
+        }
+
+        AnimatedVisibility(
+            visible = visible,
+            enter = fadeIn(animationSpec = tween(500))
+        ) {
+            Text(
+                text = "Developer Shortcuts",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
